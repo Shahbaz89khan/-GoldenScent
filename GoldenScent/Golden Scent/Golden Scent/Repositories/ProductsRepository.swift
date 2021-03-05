8c26fa63c914049d2c40dbce598dae91b198efcd
Shahbaz khan <mrcricket33@gmail.com>
2021-03-05 14:18:01 +0400

    Products Repository added in this I have added a protocol that will be implemented by Repository struct. It will bring the data from json file and it will convert in to required Business Object Struct.

---
+++ GoldenScent/Golden Scent/Golden Scent/Repositories/ProductsRepository.swift
@@ -0,0 +1,47 @@
+//
+//  ProductsRepository.swift
+//  Golden Scent
+//
+//  Created by Shahbaz Khan on 05/03/2021.
+//
+
+import UIKit
+
+protocol ProductsRepositoryProtocol {
+    func loadProducts<T: Decodable>() -> T?
+}
+struct ProductsRepository: ProductsRepositoryProtocol {
+
+    // Products file name from the resources
+    private var fileName : String
+    
+    init(fileName : String) {
+        self.fileName = fileName
+    }
+    
+    
+    func loadProducts<T: Decodable>() -> T? {
+        let data: Data
+        guard let file = Bundle.main.url(forResource: self.fileName, withExtension: nil)
+            else {
+                // Return nil if the file not found
+                return nil
+        }
+        
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            // Return nil if problem wit the data in file
+            return nil
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            // Not properly parse
+            return nil
+        }
+    }
+    
+}
