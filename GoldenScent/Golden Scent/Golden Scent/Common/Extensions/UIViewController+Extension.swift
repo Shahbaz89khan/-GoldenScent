b64987eed3a6a0a5d68514fef7e9a51505e2ae0e
Shahbaz khan <mrcricket33@gmail.com>
2021-03-06 01:34:09 +0400

    Coordinator protocol and MainCoordinator Class added that will handle the complete flow of the app. ViewController extension added

---
+++ GoldenScent/Golden Scent/Golden Scent/Common/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewController+Extension.swift
+//  Golden Scent
+//
+//  Created by Shahbaz Khan on 05/03/2021.
+//
+
+import UIKit
+
+class UIViewController_Extension: NSObject {
+
+}
---
+++ GoldenScent/Golden Scent/Golden Scent/Coordinator/MainCordinator.swift
@@ -0,0 +1,12 @@
+//
+//  MainCordinator.swift
+//  Golden Scent
+//
+//  Created by Shahbaz Khan on 05/03/2021.
+//
+
+import UIKit
+
+class MainCordinator: NSObject {
+
+}
